d519908ae0befacc916d59003e9f818d63ba31b1
chib31 <chib31@outlook.com>
2020-03-01 14:28:49 +0000

    setup

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export default {
   TEAM_NAME: 'Plastics CC',
-  BASE_URL: 'http://localhost:9090',
-  //BASE_URL: 'https://cb-heroku.herokuapp.com/api/reports',
+  //BASE_URL: 'http://localhost:9090',
+  BASE_URL: 'https://quackstats-sba.herokuapp.com/',
   STATS_API_PATH: 'api/stats?statType=',
   REQUEST_TIMEOUT: 3000,
   API_USER: 'user',
