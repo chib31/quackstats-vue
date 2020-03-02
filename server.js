47025b83f8e0175951a4ecc49920e7f0c0572b25
chib31 <chib31@outlook.com>
2020-03-02 23:36:24 +0000

    prod run mode

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-// const express = require('express');
-// const serveStatic = require("serve-static");
-// const path = require('path');
-// let app = express();
-// app.use(serveStatic(path.join(__dirname, 'dist')));
-// const port = process.env.PORT || 80;
-// app.listen(port);
+const express = require('express');
+const path = require('path');
+const serveStatic = require('serve-static');
+const app = express();
+app.use(serveStatic(__dirname + "/dist"));
+const port = process.env.PORT || 5000;
+app.listen(port);
+console.log('server started '+ port);
